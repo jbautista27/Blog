05537f65d19c5f367d46916cf038652ca05c7a74
James Bautista <jamesbautista38@gmail.com>
2020-02-06 01:41:41 +0800

    Initial commit

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Blog.Data;
+using Blog.Data.Repository;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repo;
+
+        public HomeController(IRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            var posts = _repo.GetAllPost();
+            return View(posts);
+        }
+
+        public IActionResult Post(int id)
+        {
+            var post = _repo.GetPost(id);
+            return View(post);
+        }
+      
+    }
+}
---
+++ Blog/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Blog.Data.Repository;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+                .AddEntityFrameworkStores<AppDbContext>();
+                //.AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+            });
+            services.AddTransient<IRepository, Repository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
